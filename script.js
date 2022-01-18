ad97c3fa292d7ada7ad9a0389edb0e7e79777c4a
Kyle <kylehuskey@gmail.com>
2022-01-17 21:58:28 -0500

    add some html and css style

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 const h2 = document.createElement("h2")
-h2.textContent = "My name is Kyle Huskey"
+h2.textContent = ""
 document.querySelector("body").appendChild(h2)
+
+
+function myFunction() {
+    document.getElementById("myP").style.visibility = "hidden";
+    
+  }
+  function myFunction2() {
+    document.getElementById("myP").style.visibility="visible";
+    
+  }
+
+ 
+ const h1 = document.querySelector("h1");
+
+ const theImage = document.getElementById("#theImage")
+ 
+
+ function clickAlert() {
+  alert('I was clicked!');
+}
+
+h1.addEventListener('click', clickAlert);
+theImage.addEventListener('click', clickAlert);
